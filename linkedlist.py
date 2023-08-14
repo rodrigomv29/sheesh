fb03650fe79a502e23791f6b768375e50364492d
Rodrigo Morales <rodrigomv29@gmail.com>
2023-08-14 11:45:37 -0400

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -16,22 +16,3 @@ class LinkedList:
       print(curr.data, end=" -> ")
       curr = curr.next
 
-
-if __name__ == "__main__":
-  kim = Node("kimone")
-  cici = Node("cici")
-  foreign = Node("foreignn")
-  kitkat = Node("kitkatkait")
-  mani = Node("imani")
-  chocc = Node("chocc_lit")
-  thicky = Node("thicky")
-  jaidime = Node("jaidime")
-  kim.next = cici
-  cici.next = foreign;
-  foreign.next = kitkat
-  kitkat.next = mani
-  mani.next = chocc;
-  chocc.next = thicky
-  thicky.next = jaidime
-  list = LinkedList(kim)
-  list.printList();
