5eaa6fbf6b1d3d235934c6e8df367f7a8dcc5587
Rodrigo Morales <rodrigomv29@gmail.com>
2024-11-02 16:58:38 -0400

    fixed prime function

--- isPrimeBot.py
+++ isPrimeBot.py
@@ -2,7 +2,7 @@ import math
 def isPrime(num):
     sqrt_num = math.sqrt(num)
     for i in range(2, sqrt_num):
-        if num % 2 == 0:
+        if num % i == 0:
             return False
         continue
 
