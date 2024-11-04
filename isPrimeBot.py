46a706c24a3f4770f20a4aa866ecc37320e41f22
Rodrigo Morales <rodrigomv29@gmail.com>
2024-11-03 22:38:47 -0500

    added prime bot class

--- isPrimeBot.py
+++ isPrimeBot.py
@@ -1,8 +1,11 @@
 import math
-def isPrime(num):
-    sqrt_num = math.sqrt(num)
-    for i in range(2, sqrt_num):
-        if num % i == 0:
-            return False
-        continue
+class PrimeBot:
+    def __init__(self, name):
+        self.name = name
+    def isPrime(self, num):
+        sqrt_num = math.sqrt(num)
+        for i in range(2, sqrt_num):
+            if num % i == 0:
+                return False
+            continue
 
